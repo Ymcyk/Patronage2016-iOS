0aa91e9d66aab43e09c9fd7a9145797cfe63716c
Piotr Zieliński <thepateto@gmail.com>
2016-01-18 16:14:09 +0100

    Initial Commit

---
+++ Cueter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Cueter
+//
+//  Created by Piotr Zielinski on 18.01.2016.
+//  Copyright © 2016 myApps. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
