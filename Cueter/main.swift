bfdab3b2895552839305ae8ec96fe613e6c4d9d9
Piotr Zieliński <thepateto@gmail.com>
2016-01-30 13:41:36 +0100

    Przykład użycia klas
    Poprawione wczytywanie zdjęcia

--- Cueter/main.swift
+++ Cueter/main.swift
@@ -12,15 +12,23 @@ import Cocoa
 
 let stack = FlashcardSet(theme: "Podstawy", learning: "English", known: "Polish")
 stack.addWord(Flashcard(firstPage: "Apple", secondPage: "Jabłko", hints: [Hint.Text(text: "Grows on a tree"),
-    Hint.Text(text: "You eat it"), Hint.Image(image: NSImage(byReferencingFile: "/Users/Piotr/Pictures/chin.png"), title: "Apple hint")]))
+    Hint.Text(text: "You eat it"), Hint.Image(image: NSImage(named: "data/apple.png"), title: "Apple image")]))
 
-stack.addWord(Flashcard(firstPage: "Dog", secondPage: "Pies", hints: [Hint.Image(image: NSImage(byReferencingFile: "/Users/Piotr/Pictures/chin.png"), title: "Apple hint"), Hint.Music(music: NSSound(contentsOfFile: "/Users/Piotr/phaser/resources/tutorials/04 Advanced TypeScript Projects/assets/title3.mp3", byReference: true), title: nil)]))
+stack.addWord(Flashcard(firstPage: "Dog", secondPage: "Pies", hints: [Hint.Image(image: NSImage(named: "data/dog.png"), title: "Dog image"), Hint.Music(music: NSSound(contentsOfFile: "woowwoow.mp3", byReference: true), title: "Dog barking")]))
 
+
+print(stack["Apple"]!)
+print(stack["Dog"]!)
+
+print("")
 debugPrint(stack["Apple"]!)
+print("")
 debugPrint(stack["Dog"]!)
 
+print("")
 if let bizarre = stack["bizarre"]{
     print(bizarre)
 } else {
     print("słowo nie istnieje w zbiorze")
 }
+
