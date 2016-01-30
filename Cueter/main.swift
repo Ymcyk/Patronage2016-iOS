c940379d02e6dbdfb5feba5775289f64d18dcbe7
Piotr Zieliński <thepateto@gmail.com>
2016-01-30 23:30:49 +0100

    Testowanie funkcjonalności

--- Cueter/main.swift
+++ Cueter/main.swift
@@ -8,7 +8,14 @@
 
 import Cocoa
 
-// Zadanie 2
+/*
+ *  Funkcja zwracająca listę elementów nienauczonych
+ */
+func nonLearned(learnArray: [Learnable]) -> [Learnable] {
+    return learnArray.filter({ $0.isLearned == false })
+}
+
+// Zadanie 3
 
 let stack = FlashcardSet(theme: "Podstawy", learning: "English", known: "Polish")
 stack.addWord(Flashcard(firstPage: "Apple", secondPage: "Jabłko", hints: [Hint.Text(text: "Grows on a tree"),
@@ -16,6 +23,9 @@ stack.addWord(Flashcard(firstPage: "Apple", secondPage: "Jabłko", hints: [Hint.
 
 stack.addWord(Flashcard(firstPage: "Dog", secondPage: "Pies", hints: [Hint.Image(image: NSImage(named: "data/dog.png"), title: "Dog image"), Hint.Music(music: NSSound(contentsOfFile: "woowwoow.mp3", byReference: true), title: "Dog barking")]))
 
+stack.addWord(Flashcard(firstPage: "Home", secondPage: "Dom"))
+stack.addWord(Flashcard(firstPage: "Pen", secondPage: "Długopis"))
+stack["Pen"]?.isLearned = true
 
 print(stack["Apple"]!)
 print(stack["Dog"]!)
@@ -26,9 +36,10 @@ print("")
 debugPrint(stack["Dog"]!)
 
 print("")
-if let bizarre = stack["bizarre"]{
-    print(bizarre)
-} else {
-    print("słowo nie istnieje w zbiorze")
+// zwraca listę nienauczonych elementów
+let nonLeraned = stack.flashcards.nonLearned()
+
+for element in nonLeraned {
+    print(element)
 }
 
